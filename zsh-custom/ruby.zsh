e7caba54474d6ad1f5174859b0acdd40efc5b875
Marcos Wright-Kuhns <mwrightkuhns@newrelic.com>
2020-11-30 10:01:47 -0800

    Add my bundler aliases in zsh

---
+++ zsh-custom/ruby.zsh
@@ -0,0 +1,8 @@
+# Bundler shortcuts
+# via http://ryan.mcgeary.org/2011/02/09/vendor-everything-still-applies/
+alias b="bundle"
+alias bi="b install"
+alias bil="bi --local"
+alias bu="b update"
+alias be="b exec"
+alias binit="bi && b package && bil && echo '.bundle/' >> .gitignore && echo 'vendor/cache/' >> .gitignore"
