826d341f6d518d35ad59dc7035b7ba9131c26f2c
Marcos Wright-Kuhns <webmaster@wrightkuhns.com>
2022-05-17 16:10:34 -0700

    Switch to using GOPATH known to go
    ...which for my asdf-installed version of go is a GOPATH specific to that version of golang.

--- zsh-custom/go.zsh
+++ zsh-custom/go.zsh
@@ -1,4 +1,8 @@
-if [ -d "$HOME/workspace" ]; then
+GOPATH_FROM_GO="$(go env GOPATH)"
+
+if [ $? -eq 0 ]; then
+  export GOPATH="$GOPATH_FROM_GO"
+elif [ -d "$HOME/workspace" ]; then
   GOPATH=$HOME/workspace/go
 elif [ -d "$HOME/personalspace" ]; then
   GOPATH=$HOME/personalspace/go
