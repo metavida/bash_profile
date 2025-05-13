9e65762aa693545a02df648e9cac6cfbb0f08819
Marcos Wright-Kuhns <webmaster@wrightkuhns.com>
2025-05-13 10:21:58 -0500

    Install erlang docs when I compile versions

--- zsh-custom/erlang.zsh
+++ zsh-custom/erlang.zsh
@@ -2,3 +2,4 @@
 # https://github.com/asdf-vm/asdf-erlang#use
 # https://dev.to/andresdotsh/how-to-install-erlang-on-macos-with-asdf-3p1c
 export KERL_CONFIGURE_OPTIONS="--without-javac --with-ssl=$(brew --prefix openssl)"
+export KERL_BUILD_DOCS=yes
