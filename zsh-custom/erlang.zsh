31fdfe1636fce9f094149507ed13fbbd80b31a62
Marcos Wright-Kuhns <webmaster@wrightkuhns.com>
2026-07-13 12:19:20 -0700

    Tweak KERL flags to use

--- zsh-custom/erlang.zsh
+++ zsh-custom/erlang.zsh
@@ -1,5 +1,5 @@
 # kerl build/install options
 # https://github.com/asdf-vm/asdf-erlang#use
 # https://dev.to/andresdotsh/how-to-install-erlang-on-macos-with-asdf-3p1c
-export KERL_CONFIGURE_OPTIONS="--without-javac --with-ssl=$(brew --prefix openssl)"
+export KERL_CONFIGURE_OPTIONS="--with-ssl=$(brew --prefix openssl) --without-javac --without-wx --without-odbc"
 export KERL_BUILD_DOCS=yes
